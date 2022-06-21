38d5a99509a9cc11815ca162b7abd5cea19c86a3
aphrysof <sophieabdillahi65@gmail.com>
2022-06-21 12:20:53 +0300

    search-functionality

--- src/components/navbar/Index.js
+++ src/components/navbar/Index.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React,  {useState} from "react";
 import "./navbar.css";
 import logo from "../../assets/logo.png";
 import { Link } from "react-router-dom";
 
 const Index = () => {
+
+  const [userInput, setUserInput] = useState("");
+
+  const handleChange = (e) => {
+      setUserInput(e.target.value);
+  }
+
+  const handleSubmit =(e) => {
+    e.preventDefault();
+    fetch(`https://api.github.com/users/${userInput}`)
+    .then((res) => 
+      res.json()
+    ).then ((data) => {
+      console.log(data)
+    })
+  }
   return (
     <nav>
       <div>
@@ -11,10 +27,10 @@ const Index = () => {
           <img src={logo} alt="logo" />
         </Link>
       </div>
-      <div className="nav--searchInput">
-        <input type="search" name="search" />
+      <form className="nav--searchInput" onSubmit = {handleSubmit}>
+        <input type="text" name="search" onChange={handleChange} />
         <button className="search--button">Search</button>
-      </div>
+      </form>
     </nav>
   );
 };
